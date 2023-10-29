21d7047c1a4f0b0d08ad3b683b1a943ef6b1758a
TheFlyingPiano99 <simonzolika1999@gmail.com>
2023-10-29 17:30:59 +0100

    Pause menu

--- Assets/Scripts/GameControllerScript.cs
+++ Assets/Scripts/GameControllerScript.cs
@@ -5,6 +5,7 @@ using TMPro;
 using System.Drawing;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms;
 
 public class GameControllerScript : MonoBehaviour
 {
@@ -20,6 +21,7 @@ public class GameControllerScript : MonoBehaviour
     private GameObject player;
     public GameObject gameOverScreen;
     public GameObject levelFinishScreen;
+    public GameObject pausedScreen;
     public TMP_Text growthProgressText;
     public GameObject blackScreen;
     private GameState gameState;
@@ -51,6 +53,22 @@ public class GameControllerScript : MonoBehaviour
         }
     }
 
+    private void checkInputInPause()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            gameState = GameState.GameRunning;
+            Time.timeScale = 1.0f;
+            pausedScreen.SetActive(false);
+        }
+        else if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Time.timeScale = 1.0f;
+            pausedScreen.SetActive(false);
+            Application.Quit();
+        }
+    }
+
     public void Update()
     {
         timeSpentInState += Time.deltaTime;
@@ -60,9 +78,13 @@ public class GameControllerScript : MonoBehaviour
                 waitForInputAndRestartScene();
                 break;
             case GameState.GamePaused:
-
+                checkInputInPause();
                 break;
             case GameState.GameRunning:
+                if (Input.GetKeyDown(KeyCode.Escape))
+                {
+                    transitionToPaused();
+                }
                 break;
             case GameState.LevelFinished:
                 waitForInputAndRestartScene();
@@ -78,6 +100,7 @@ public class GameControllerScript : MonoBehaviour
         gameState = GameState.GameRunning;
         gameOverScreen.SetActive(false);
         levelFinishScreen.SetActive(false);
+        pausedScreen.SetActive(false);
         blackScreen.GetComponent<CanvasRenderer>().SetAlpha(1.0f);
         isFadeInFromBlack = true;
         winAudio.Stop();
@@ -118,6 +141,18 @@ public class GameControllerScript : MonoBehaviour
         ambientMusic.Stop();
         winAudio.Play();
         Debug.Log("Level finished.");
+    }
+
+    public void transitionToPaused()
+    {
+        if (gameState == GameState.GamePaused)
+            return;
+
+        gameState = GameState.GamePaused;
+        timeSpentInState = 0.0;
+        Time.timeScale = 0.0f;
+        pausedScreen.SetActive(true);
+        Debug.Log("Paused.");
     }
 
     private void fadeInFromBlack()
